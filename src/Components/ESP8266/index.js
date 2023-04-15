da566b6fe154261dbf5088b425f541ca69a63067
PhamLo <phamlo200102@gmail.com>
2023-04-15 12:00:54 +0700

    apply firebase

--- src/Components/ESP8266/index.js
+++ src/Components/ESP8266/index.js
@@ -1,11 +1,32 @@
-import { ref } from "firebase/database"
+import { child, get, ref } from "firebase/database"
 import { database } from "../../utils/firebaseConfig"
+import { useEffect, useState } from "react"
 
 const APPESP = () => {
+    const dbRef = ref(database)
+    const [NhietDo, setNhietDo] = useState(0)
 
-   const dbRef = ref(database)
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            get(child(dbRef,'Temperature')).then((data) => {
+                if (data.exists()) {
+                    setNhietDo(data.val())
+                }
+                else {
+                    console.log(`no data`);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        },3000)
+
+        return () => clearInterval(id)
+    },[])
+
     return (
-        <div style={{color:"black"}}>ng</div>
+        <div style={{color:"black"}}>{NhietDo}</div>
     )
 }
 
